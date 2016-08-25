43a01ad8da6483ba38cd8ae140392c799b66993c
spectrecode <you@example.com>
2016-08-25 15:28:33 -0500

    promedio

---
+++ main.js
@@ -0,0 +1,5 @@
+var num1 = prompt ("agrega un número");
+var num2 = prompt ("agrega otro número");
+var num3 = prompt ("agrega un número más");
+var promedio = (parseInt(num1) + parseInt(num2) + parseInt(num3))/ 3;
+window.alert("El promedio de ambos números es " + promedio);
---
+++ readme.md
@@ -0,0 +1,12 @@
+#SUMA
+Procedimiento de sumar 2 numeros con javscript
+* inicio
+* Leer 
+    * dato 1
+    * dato 2
+    * dato 3
+* Promediar datos
+* Entregar promedio
+* Fin
+
+![con titulo](http://i65.tinypic.com/312yfli.jpg"diagrama")
